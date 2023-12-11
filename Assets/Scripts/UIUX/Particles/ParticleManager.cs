bb80a94711787e143813bb5971bd77a52c9fc2c2
SNIELSEL <niels@haverkotte.nl>
2023-12-11 16:44:02 +0100

    gearSwapParticle added && VR compatible in UI Scene

--- Assets/Scripts/UIUX/Particles/ParticleManager.cs
+++ Assets/Scripts/UIUX/Particles/ParticleManager.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class ParticleManager : MonoBehaviour
 {
-
+    public GameObject[] exhausts;
     public void PlayParticle(ParticleSystem particle)
     {
         particle.Play();
+    }
+
+    public void PlayExhaust()
+    {
+        for (int i = 0; i < exhausts.Length; i++)
+        {
+            exhausts[i].GetComponent<ParticleSystem>().Play();
+        }
     }
 }
--- Assets/Scripts/UIUX/PropEnabler.cs
+++ Assets/Scripts/UIUX/PropEnabler.cs
@@ -7,15 +7,58 @@ public class PropEnabler : MonoBehaviour
     public GameObject[] particleObject;
     public GameObject[] probObject;
 
-    public void EnableParticleAndProp(int number)
+    public GameObject exhaust;
+
+    private int enabledProps;
+    private bool propsEnabled;
+    private float oneSecDelay;
+
+    public virtual void EnableParticleAndProp(int number)
     {
-        particleObject[number].SetActive(true);
-        probObject[number].SetActive(true);
+        if (!propsEnabled)
+        {
+            propsEnabled = true;
+            enabledProps = number;
+            oneSecDelay = 1;
+
+            if (particleObject[number] != null)
+            {
+                particleObject[number].SetActive(true);
+
+                if(particleObject[number].name == exhaust.name)
+                {
+                    particleObject[number+ 1].SetActive(true);
+                }
+            }
+
+            if (probObject[number] != null)
+            {
+                probObject[number].SetActive(true);
+            }
+        }
     }
 
-    public void DisableParticleAndProp(int number)
+    private void Update()
     {
-        particleObject[number].SetActive(false);
-        probObject[number].SetActive(false);
+        if (propsEnabled)
+        {
+            oneSecDelay -= Time.deltaTime;
+
+            if(oneSecDelay <= 0)
+            {
+                if (!particleObject[enabledProps].GetComponent<ParticleSystem>().isPlaying)
+                {
+                    DisableParticleAndProp();
+                }
+            }
+        }
+    }
+
+    public void DisableParticleAndProp()
+    {
+        particleObject[enabledProps].SetActive(false);
+        probObject[enabledProps].SetActive(false);
+
+        propsEnabled = false;
     }
 }
