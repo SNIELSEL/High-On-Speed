1f6e01f771fe45446f0499c67369cd3f5e25ff59
SNIELSEL <niels@haverkotte.nl>
2023-12-13 12:34:14 +0100

    TireSmoke particicle done && Vr Compatible in Ui Scene && UI folder Cleanup

--- Assets/Scripts/UIUX/PropEnabler.cs
+++ Assets/Scripts/UIUX/PropEnabler.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using static UnityEngine.ParticleSystem;
 
 public class PropEnabler : MonoBehaviour
 {
@@ -9,16 +11,28 @@ public class PropEnabler : MonoBehaviour
 
     public GameObject exhaust;
 
-    private int enabledProps;
-    private bool propsEnabled;
-    private float oneSecDelay;
+    private ParticleSystem individualParticle;
+    private GameObject individualProp;
 
-    public virtual void EnableParticleAndProp(int number)
+    private int enabledProps;
+    private float oneSecDelay;
+    private float loopDisableDelay;
+    private bool propsEnabled;
+    private bool usingIndividualEnabler;
+    private bool usingLoopParticle;
+
+    public void EnableParticleAndProp(int number)
     {
+        if (particleObject[enabledProps].GetComponent<ParticleSystem>().main.loop == true)
+        {
+            usingLoopParticle = true;
+        }
+
         if (!propsEnabled)
         {
             propsEnabled = true;
             enabledProps = number;
+            loopDisableDelay = 5;
             oneSecDelay = 1;
 
             if (particleObject[number] != null)
@@ -38,20 +52,74 @@ public class PropEnabler : MonoBehaviour
         }
     }
 
+    public void IdividualPropEnabler(GameObject prop)
+    {
+        prop.SetActive(true);
+        individualProp = prop;
+    }
+
+    public void IdividualParticleEnabler(ParticleSystem particle)
+    {
+        if(particle.main.loop == true)
+        {
+            usingLoopParticle = true;
+        }
+
+        if (!propsEnabled)
+        {
+            propsEnabled = true;
+            oneSecDelay = 1;
+            loopDisableDelay = 5;
+            individualParticle = particle;
+            usingIndividualEnabler = true;
+            particle.gameObject.SetActive(true);
+        }
+    }
+
     private void Update()
     {
         if (propsEnabled)
         {
+            loopDisableDelay -= Time.deltaTime;
             oneSecDelay -= Time.deltaTime;
 
-            if(oneSecDelay <= 0)
+            if(oneSecDelay <= 0 && !usingIndividualEnabler && !usingLoopParticle)
             {
                 if (!particleObject[enabledProps].GetComponent<ParticleSystem>().isPlaying)
                 {
                     DisableParticleAndProp();
                 }
             }
+
+            if(oneSecDelay <= 0 && usingIndividualEnabler && !usingLoopParticle)
+            {
+                if (!individualParticle.isPlaying)
+                {
+                    DisableIndividualParticleAndProp();
+                }
+            }
+
+            if (loopDisableDelay <= 0 && usingLoopParticle)
+            {
+                if (usingIndividualEnabler)
+                {
+                    DisableIndividualParticleAndProp();
+                }
+                else if (!usingIndividualEnabler)
+                {
+                    DisableParticleAndProp();
+                }
+            }
         }
+    }
+    public void DisableIndividualParticleAndProp()
+    {
+        individualParticle.gameObject.SetActive(false);
+        individualProp.gameObject.SetActive(false);
+
+        usingLoopParticle = false;
+        propsEnabled = false;
+        usingIndividualEnabler = false;
     }
 
     public void DisableParticleAndProp()
@@ -59,6 +127,7 @@ public class PropEnabler : MonoBehaviour
         particleObject[enabledProps].SetActive(false);
         probObject[enabledProps].SetActive(false);
 
+        usingLoopParticle = false;
         propsEnabled = false;
     }
 }
